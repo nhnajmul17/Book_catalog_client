2ded847434aa3df44ddb10dcf75491b81eac2312
nhnajmul17 <nhnajmul17@gmail.com>
2023-07-15 11:34:46 +0600

    Navbar and Login Page created

---
+++ src/layouts/Navbar.tsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-16 fixed top backdrop-blur-lg z-10">
+      <div className="h-full w-full bg-white/60">
+        <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
+          <div>
+            <Link to="/products">
+              <img className="h-8" src="" alt="logo" />
+            </Link>
+          </div>
+          <div>
+            <ul className="flex items-center">
+              <li>
+                <Link to="/products">
+                  <button className="py-2 px-4 mx-4 bg-blue-500 text-white font-semibold rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                    All Books
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard">
+                  <button className="py-2 mx-2 px-4 bg-blue-500 text-white font-semibold rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                    Dashboard
+                  </button>
+                </Link>
+              </li>
+              {/* <li>
+                  <button>
+                    <Link to="/cart">
+                      <HiOutlineShoppingCart size="25" />
+                    </Link>
+                  </button>
+                </li> */}
+            </ul>
+          </div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">User Name</a>
+              </li>
+              <li></li>
+              <li>
+                <Link to="/login">
+                  <a>Login</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,62 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+interface LoginFormInputs {
+  email: string;
+  password: string;
+}
 const Login = () => {
-  return <div>this is login</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: LoginFormInputs) => {
+    console.log(data);
+    // dispatch(SigninUser({ email: data.email, password: data.password })).then(
+    //     navigate(from, { replace: true })
+    // )
+  };
+  return (
+    <div className="h-[500px] flex flex-col justify-center items-center py-2">
+      <h2 className="text-4xl">Login</h2>
+      {/* {isError ? <p className="text-red-500">{error}</p> : ""} */}
+      <div className="w-80">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email")}
+              placeholder="Your Email"
+              className="input input-bordered w-full "
+            />
+          </div>
+          <div className="form-control w-full  my-2">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Your Password"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <button type="submit" className="btn btn-accent w-full my-2">
+            Submit
+          </button>
+        </form>
+      </div>
+      <p>
+        Don't Have an account?{" "}
+        <Link to="/signup" className="text-blue-800">
+          Create now.
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
